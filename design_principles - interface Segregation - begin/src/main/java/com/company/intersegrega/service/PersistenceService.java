d54ab433405d103665273e17cf2a834a792249e9
Oscar Calzada <noctaelux@gmail.com>
2022-08-14 17:23:48 -0500

    Interface Segregation Principle (ISP)

--- design_principles - interface Segregation - begin/src/main/java/com/company/intersegrega/service/PersistenceService.java
+++ design_principles - interface Segregation - begin/src/main/java/com/company/intersegrega/service/PersistenceService.java
@@ -8,10 +8,8 @@ import com.company.intersegrega.entity.Entity;
 public interface PersistenceService<T extends Entity> {
 
 	public void save(T entity);
-	
+
 	public void delete(T entity);
-	
+
 	public T findById(Long id);
-	
-	public List<T> findByName(String name);
 }
